086cc01f94086005776da33a1e0a8a22a3950ad2
Aarni <aarni.miikkulainen@helsinki.fi>
2022-03-25 14:41:35 +0200

    raportointijuttuja

--- laskarit/Viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/Viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,26 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    
+    def test_aloitussaldo_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+
+    def test_saldon_kasvattaminen_toimii(self):
+        self.maksukortti.lataa_rahaa(10)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.2")
+
+    def test_saldo_vähenee_oikein_maksusta(self):
+        self.maksukortti.lataa_rahaa(190)
+        self.maksukortti.ota_rahaa(100)
+        self.assertEqual(str(self.maksukortti),"saldo: 1.0")
+
+    def test_saldo_liian_pieni(self):
+        self.maksukortti.ota_rahaa(1000)
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+    
+    def test_rahat_riittävät(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(0.05), True)
+
+    def test_rahat_eivät_riitä(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(100), False)
+        
