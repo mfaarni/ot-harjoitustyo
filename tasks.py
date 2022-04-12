916b6c39dcc971b1ca478aa5de735d9778ee5625
mfaarni <102048170+mfaarni@users.noreply.github.com>
2022-04-12 22:07:46 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -12,7 +12,9 @@ def start(ctx):
 def test(ctx):
 	ctx.run("pytest src", pty=True)
 	
-
+@task
+def lint(ctx):
+	ctx.run("pylint src", pty=True)
 @task
 def coverage(ctx):
 	ctx.run("coverage run --branch -m pytest src", pty=True)
