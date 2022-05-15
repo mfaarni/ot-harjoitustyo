435caa9e352b120e29b807368499c997182f5668
mfaarni <102048170+mfaarni@users.noreply.github.com>
2022-05-15 17:29:51 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -7,20 +7,20 @@ from invoke import task
 
 def start(ctx):
 	ctx.run("python3 src/menu.py", pty=True)
-	
+@task	
 def build(ctx):
 	ctx.run("python3 src/database/initialize_database.py", pty=True)
 	
 @task
 def test(ctx):
-	ctx.run("pytest src --ignore src/test_main_game.py", pty=True)
+	ctx.run("pytest src --ignore src/tests/*", pty=True)
 	
 @task
 def lint(ctx):
 	ctx.run("pylint src", pty=True)
 @task
 def coverage(ctx):
-	ctx.run("coverage run --branch -m pytest src --ignore src/test_main_game.py", pty=True)
+	ctx.run("coverage run --branch -m pytest src --ignore src/tests/*", pty=True)
     
 @task(coverage)
 def coverage_report(ctx):
